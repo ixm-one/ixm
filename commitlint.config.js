deeb60ad52b716cebe00609987b5eab26db6c6ba
Izzy Muerte <63051+bruxisma@users.noreply.github.com>
2025-03-17 04:04:54 -0700

    ci(actions): build documentation when a PR is opened (#43)
    After the recent deployment failure, checking to make sure documentation
    at least builds if any file under `docs/` is built is worth the time we
    run the task for. Currently using the builtin `paths` argument, but
    might switch to the skip-duplicate-actions workflow so that we can mark
    this workflow as required.
    
    Additionally, we're not doing anything with the output, we just want to
    make sure there aren't any build errors from vitepress at this time. If
    we move to starlight in the future, we'll most likely keep this same
    approach/workflow.
    
    ---------
    
    Signed-off-by: Izzy Muerte <63051+bruxisma@users.noreply.github.com>

--- commitlint.config.js
+++ commitlint.config.js
@@ -3,8 +3,5 @@ import { RuleConfigSeverity } from "@commitlint/types";
 export default {
   extends: ["@commitlint/config-conventional"],
   defaultIgnores: true,
-  rules: {
-    "subject-case": [RuleConfigSeverity.Error, `always`, [`lower-case`]],
-    "scope-case": [RuleConfigSeverity.Error, `always`, [`kebab-case`]],
-  },
+  rules: { "scope-case": [RuleConfigSeverity.Error, `always`, [`kebab-case`]] },
 };
