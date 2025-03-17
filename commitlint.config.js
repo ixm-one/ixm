a2117a885c7f5e378236766584de7b89390631e4
Izzy Muerte <63051+bruxisma@users.noreply.github.com>
2025-03-17 03:46:08 -0700

    chore(config): reduce commitlint and renovate details (#42)
    It's easier to just rely on the `type-enum` and change the scope in the
    title of PRs from myself or others than to deal with it as a hard error
    requirement. This also means we'll go back to `chore(deps)` but I'm fine
    with that. We don't want those to show up in our changelog once we start
    using `git-cliff` anyhow.
    
    Signed-off-by: Izzy Muerte <63051+bruxisma@users.noreply.github.com>

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,13 +1,10 @@
 import { RuleConfigSeverity } from "@commitlint/types";
 
-const scopes = [`actions`, `cmake`, `docs`, `package`, `tool`];
-
 export default {
   extends: ["@commitlint/config-conventional"],
   defaultIgnores: true,
   rules: {
     "subject-case": [RuleConfigSeverity.Error, `always`, [`lower-case`]],
     "scope-case": [RuleConfigSeverity.Error, `always`, [`kebab-case`]],
-    "scope-enum": [RuleConfigSeverity.Error, `always`, scopes],
   },
 };
